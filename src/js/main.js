97e36f1afcf5916824a739d79c6443293979d0cf
AndreLucyo2 <andre.lucyo2@gmail.com>
2024-04-02 22:19:05 -0300

    estudo 01 login form

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,38 @@
+
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const emailError = document.getElementById('emailError');
+    const passwordError = document.getElementById('passwordError');
+
+    // Limpar mensagens de erro anteriores
+    emailError.textContent = '';
+    passwordError.textContent = '';
+
+    //Validação do e-mail:
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailValid = emailPattern.test(emailInput.value);
+
+    if (!emailValid) {
+        emailError.textContent = 'Digite um e-mail válido.';
+    }
+
+    //Validação da senha:
+    if (passwordInput.value.length < 6) {
+        passwordError.textContent = 'A senha deve conter pelo menos 6 caracteres.\nInforme:123456';
+    }
+
+    // Simulação de autenticação - Aqui você pode verificar as credenciais com um servidor backend
+    if (emailValid && passwordInput.value.length >= 6) {
+        alert('Login realizado com sucesso!');
+
+        // Aqui você pode redirecionar
+    }
+
+});
+
+
 //---------------------------------------------------------------------
 //Estudo de elementos HTML
 //---------------------------------------------------------------------
@@ -12,7 +47,7 @@ alementBtnAdd.addEventListener('click', function () {
         window.alert("OK,vou fechar esta pagina!");
 
         //Fecha a aba corrente:
-        close();        
+        close();
     }
 
 });
